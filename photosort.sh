30b065474b3852f4a42986b391a58a6a0f82e238
shamandar <73035147+shamandar@users.noreply.github.com>
2022-02-18 18:23:49 -0500

    Add UNSORTED_DIR argument. Use absolute directory paths

--- photosort.sh
+++ photosort.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
-if [ $# != 2 ]; then
-    echo "Usage: $0 INPUT_DIR SORTED_DIR"
+if [ $# != 3 ]; then
+    echo "Usage: $0 INPUT_DIR SORTED_DIR UNSORTED_DIR"
     echo
-    echo "Copy files from INPUT_DIR to SORTED_DIR by Exif Date/Time"
+    echo "Sort INPUT_DIR files by Exif DateTime and Copy them to SORTED_DIR."
+    echo "Files missing Exif DateTime are copied to UNSORTED_DIR."
     exit 1
 fi
 
-input_dir=$1
-sorted_dir=$2
+input_dir=$(readlink -f $1)
+sorted_dir=$(readlink -f $2)
+unsorted_dir=$(readlink -f $3)
+
+# NOTE: `exiftool` copy file missing Exif to current directory
+[ -d $unsorted_dir ] || mkdir -p $unsorted_dir
+cd $unsorted_dir
 
 # NOTE: Option `-o .` means Copy file instead default Rename file
 exiftool -o . '-FileName<CreateDate' -d "$sorted_dir/%Y/%m/%d/%Y%m%d_%H%M_%%f.%%e" -r $input_dir
