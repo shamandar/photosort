edfff8bc706b9a6139952264d88b90b6264dd0af
shamandar <73035147+shamandar@users.noreply.github.com>
2022-02-19 22:27:55 -0500

    KISS: Rename files based on CreateDate. Remove UNSORTED_DIR argument

--- photosort.sh
+++ photosort.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
 
-if [ $# != 3 ]; then
-    echo "Usage: $0 INPUT_DIR SORTED_DIR UNSORTED_DIR"
+if [ $# != 2 ]; then
+    echo "Usage: photosort.sh INPUT_DIR SORTED_DIR"
     echo
-    echo "Sort INPUT_DIR files by Exif DateTime and Copy them to SORTED_DIR."
-    echo "Files missing Exif DateTime are copied to UNSORTED_DIR."
+    echo "Rename media files from INPUT_DIR to SORTED_DIR based on CreateDate."
+    echo "Non-media and media files missing CreateDate remain unchanged in INPUT_DIR."
     exit 1
 fi
 
 input_dir=$(readlink -f $1)
 sorted_dir=$(readlink -f $2)
-unsorted_dir=$(readlink -f $3)
 
-# NOTE: `exiftool` copy file missing Exif to current directory
-[ -d $unsorted_dir ] || mkdir -p $unsorted_dir
-cd $unsorted_dir
-
-# NOTE: Option `-o .` means Copy file instead default Rename file
-exiftool -o . '-FileName<CreateDate' -d "$sorted_dir/%Y/%m/%d/%Y%m%d_%H%M_%%f.%%e" -r $input_dir
+exiftool '-FileName<CreateDate' -d "$sorted_dir/%Y/%m/%d/%Y%m%d_%H%M_%%f.%%e" -r $input_dir
